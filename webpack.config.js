e1283d80635890ad34097576cff859479babed9c
satooon <lucklaw9210@gmail.com>
2018-01-23 17:41:40 +0900

    serverless + webpack + babel

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      include: __dirname,
+      exclude: /node_modules/,
+    }]
+  },
+  externals: {
+    'aws-sdk': 'aws-sdk'
+  }
+};
